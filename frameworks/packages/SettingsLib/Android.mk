53d0f9263ba2fad01d54c70f18667cf1d9eb65c5
Alex Cruz <mazdarider23@gmail.com>
2016-01-06 05:27:58 +0000

    Theme Resources updated!

--- frameworks/packages/SettingsLib/Android.mk
+++
@@ -1,9 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-LOCAL_MODULE := SettingsLib
-
-LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/res
-LOCAL_SRC_FILES := $(call all-java-files-under, src)
-
-include $(BUILD_STATIC_JAVA_LIBRARY)
